1a02d1f8d8923db7763d5844d63346d8c7e47f7d
Donghao Wu <donghao.wu.us@gmail.com>
2020-06-21 18:40:13 -0400

    finish general function testing

---
+++ Testing/Testing-function/demo-app/script.test.js
@@ -0,0 +1,25 @@
+const googleSearch = require('./script')
+
+const dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'nike.com',
+    'dogpictures.com',
+    'myfavouritedogs.com'
+]
+
+describe('googleSearch', () => {
+    it('is searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com', 'myfavouritedogs.com'])
+    })
+
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([])
+    })
+
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
+})
